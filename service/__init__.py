d7e296b210133f14ba412ae8eeabb94b9e45d74f
Prathamesh__ <prathameshjorve09@gmail.com>
2026-09-23 09:49:51 +0530

    Initialize Flask app with CORS and Talisman

---
+++ service/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_cors import CORS
+from flask_talisman import Talisman
+
+app = Flask(__name__)
+
+CORS(app)
+Talisman(app)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
